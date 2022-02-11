d90cc432be5ec4533ff4b74844044cf0aeb582a7
Alisher <a.aituarov@ibreal.dev>
2022-02-11 15:33:04 +0600

    feature: genre routes implemented

--- backend/index.ts
+++ backend/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { PrismaClient } from '@prisma/client';
 import { authRouter } from './src/routes/auth';
 import { difficultyRouter } from './src/routes/difficulty';
+import { genreRouter } from './src/routes/genre';
 
 const prisma = new PrismaClient();
 const app = express();
@@ -10,6 +11,7 @@ app.use(express.json());
 
 app.use('/auth', authRouter);
 app.use('/difficulty', difficultyRouter);
+app.use('/genre', genreRouter);
 
 const PORT = 4000;
 
